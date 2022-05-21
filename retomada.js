379fb009935bde136d949c431996e71cafa8229e
sandrohrj <sandrohrj@gmail.com>
2022-05-21 00:39:46 -0300

    Projeto retomada, novo projeto.

---
+++ retomada.js
@@ -0,0 +1,36 @@
+function img1(){
+    var img = document.querySelector("#img10")
+    img.setAttribute('src', 'img1.jpeg')
+}
+
+function img02(){
+    
+}
+
+function img03(){
+    
+}
+
+function img04(){
+    
+}
+
+function img05(){
+    
+}
+
+function img06(){
+    
+}
+
+function img07(){
+    
+}
+
+function img08(){
+    
+}
+
+function img09(){
+    
+}
